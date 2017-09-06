179a2abf8811e6978bfba04998749aa6c76e06cc
CeciliaRodriguez <ceciliazgr@gmail.com>
2017-09-06 14:56:43 -0300

    Fixes #1

--- static/javascript/Game.js
+++ static/javascript/Game.js
@@ -3,24 +3,24 @@ var state = new GameState();
 var current_room = state.getCurrentRoom();
 
 var man_pages = {
-"mv": "\nEl comando mv sirve para modificar el nombre de archivos y directorios o \n" + 
+"mv": "\nEl comando mv sirve para modificar el nombre de archivos y directorios o \n" +
 "para trasladarlos a alguna otra ubicación dentro del árbol de directorios.\n\n"  +
 "La instrucción mv se parece mucho a cp, excepto que borra el archivo \n" +
-"original después de copiarlo. Su sintaxis es la siguiente:\n\n" + 
-"                   mv -opciones origen destino\n", 
-"mkdir": "\nEl comando mkdir, sirve para crear nuevos directorios y por lo tanto \n" + 
+"original después de copiarlo. Su sintaxis es la siguiente:\n\n" +
+"                   mv -opciones origen destino\n",
+"mkdir": "\nEl comando mkdir, sirve para crear nuevos directorios y por lo tanto \n" +
 "agrandar y mantener el orden en la estructura del sistema de archivos de Linux.\n\n"  +
 "El comando consta de la siguiente sintaxis: \n\n" +
 "                   mkdir -opciones directorio/s\n",
-"rmdir": "\nEl comando rmdir, sirve para eliminar directorios vacíos y existentes. \n" + 
+"rmdir": "\nEl comando rmdir, sirve para eliminar directorios vacíos y existentes. \n" +
 "Podés especificar uno o mas directorios para eliminar, y podés expresarlos en rutas.\n\n"  +
 "absolutas o relativas. El comando consta de la siguiente sintaxis: \n\n" +
 "                   rmdir -opciones directorio/s\n",
-"rm": "\nEl comando rm, sirve para eliminar archivos o directorios. \n" + 
+"rm": "\nEl comando rm, sirve para eliminar archivos o directorios. \n" +
 "Podés especificar uno o mas archivos para eliminar, y podés expresarlos en rutas.\n\n"  +
 "absolutas o relativas. El comando consta de la siguiente sintaxis: \n\n" +
 "                   rm -opciones archivo/s\n",
-"scp": "\nEl comando scp, sirve para copiar archivos de un ordenador a otro a través de. \n" + 
+"scp": "\nEl comando scp, sirve para copiar archivos de un ordenador a otro a través de. \n" +
 "una conexión segura y encriptada.\n\n"  +
 "Se puede utilizar scp para copiar archivos de un ordenador local a otro remoto, también\n" +
 "se puede copiar del remoto al local y también se puede copiar entre dos remotos." +
@@ -29,37 +29,37 @@ var man_pages = {
 "         scp archivo_origen user@ip_host:/ruta_dir_destino\n\n" +
 "      2. Copiar un archivo de un ordenador remoto al ordenador local:\n" +
 "         scp user@ip_host:/ruta_archivo_origen ruta_dir_destino\n\n",
-"cp": "\nEl comando cp permite copiar archivos y directorios. \n" + 
-"El comando acepta como argumentos una fuente y un destino, de forma que el primer parámetro\n" + 
+"cp": "\nEl comando cp permite copiar archivos y directorios. \n" +
+"El comando acepta como argumentos una fuente y un destino, de forma que el primer parámetro\n" +
 "de cp es el nombre del archivo que hay copiar y el segundo es el lugar donde se desea guardar\n" +
 "la copia.\n\n"  +
 "El comando consta de la siguiente sintaxis: \n\n" +
 "                   cp -opciones fuente destino\n",
-"grep": "\nEl comando grep permite buscar palabras en un archivo. \n" + 
-"El comando acepta como argumentos la palabra a buscar (o un patrón) y el archivo.\n" + 
+"grep": "\nEl comando grep permite buscar palabras en un archivo. \n" +
+"El comando acepta como argumentos la palabra a buscar (o un patrón) y el archivo.\n" +
 "El comando consta de la siguiente sintaxis: \n\n" +
 "                   grep palabra archivo\n",
-"tail": "\nEl comando tail sirve para mostrar en pantalla las últimas líneas de un archivo. \n" + 
+"tail": "\nEl comando tail sirve para mostrar en pantalla las últimas líneas de un archivo. \n" +
 "Por defecto se muestran las últimas 10 líneas, pero este número puede variar, según.\n\n"  +
 "como se ejecuten las opciones. Su sintaxis es la siguiente:\n\n" +
 "                   tail -opciones archivo\n" +
-"Por ejemplo, tail -2 file.txt mostrará las últimas dos líneas de file.txt\n", 
-"head": "\nEl comando head sirve para mostrar en pantalla las primeras líneas de un archivo. \n" + 
+"Por ejemplo, tail -2 file.txt mostrará las últimas dos líneas de file.txt\n",
+"head": "\nEl comando head sirve para mostrar en pantalla las primeras líneas de un archivo. \n" +
 "Por defecto se muestran las primeras 10 líneas, pero este número puede variar, según.\n\n"  +
 "como se ejecuten las opciones. Su sintaxis es la siguiente:\n\n" +
 "                   head -opciones archivo\n" +
 "Por ejemplo, head -8 file.txt mostrará las primeras ocho líneas de file.txt\n",
-"cd": "\nEste comando se usa para cambiar de directorio. \n" + 
+"cd": "\nEste comando se usa para cambiar de directorio. \n" +
 "Generalmente cuando el usuario inicia sesión, el directorio donde comienza es su home.\n"  +
 "Desde ahí uno puede moverse a otros directorios donde se tenga acceso usando este comando.\n" +
 "Si se ejecuta 'cd ..' se vuelve un directorio atrás y 'cd' regresa al home del usuario.\n" +
 "Su sintaxis es la siguiente:\n\n" +
 "                   cd directorio\n\n",
-"ls": "\nEl comando ls sirve para listar el contenido de los directorios. \n" + 
+"ls": "\nEl comando ls sirve para listar el contenido de los directorios. \n" +
 "Si se ejecuta sin ningún parámetro muestra el contenido del directorio actual.\n\n"  +
 "Su sintaxis es la siguiente:\n\n" +
 "                   ls -opciones directorio\n\n",
-"pwd": "\nEl comando pwd sirve para saber el directorio en el que estoy parado. \n" + 
+"pwd": "\nEl comando pwd sirve para saber el directorio en el que estoy parado. \n" +
 "También podemos saber esto mirando el prompt de la shell.\n\n"  +
 "Su sintaxis es la siguiente:\n\n" +
 "                   pwd\n\n"
@@ -76,25 +76,25 @@ $(document).ready(function() {
         var args = split.splice(1,split.length);
 
         if(current_room.commands.indexOf(command) > -1 ){
-                
+
                 current_room[command](args,term);
 
                     if (command in current_room.cmd_text){
-                         term.echo(current_room.cmd_text[command]);                  
-                    }         
+                         term.echo(current_room.cmd_text[command]);
+                    }
         }
         else{
             term.echo(command+": No se encontró la orden.");
         }
-    }, { history: true,                
-        prompt: 'mario@utnso:~$ ',     
-        name: 'Mario-Comando',         
+    }, { history: true,
+        prompt: 'mario@utnso:~$ ',
+        name: 'MarioBash',
         greetings:"",
         exit: false,
         clear: true,
-        height: 400
+        //height: 400
         });
-    
+
     // Clear history on page reload
     $("#term").terminal().history().clear();
     // terminal starts blocked until toad finishes
@@ -111,7 +111,7 @@ $(document).ready(function() {
             var last_arg = split_command.pop();
             //Start in a room, try to move through path, and if we get to the end
             // check whether a room/item could complete our trip
-            
+
             //Get starting room
             var search_room;
             if(last_arg.substring(0,1) == "~"){
@@ -163,7 +163,7 @@ $(document).ready(function() {
                                 }
                             }
                         }
-                        
+
                         //If one match exists
                         if(substring_matches.length == 1){
                             path_rooms.pop();
